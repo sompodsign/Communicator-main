ecf2eb2741153a161171e20bd4c3be61ea575f88
mss-qups <mss.qups@gmail.com>
2021-10-27 16:50:43 +0600

    name update

--- Test/testcase/whatsapp/test_whatsapp_01_send_message.py
+++ Test/testcase/whatsapp/test_whatsapp_01_send_message.py
@@ -5,7 +5,7 @@ from testcase.whatsapp.base_test_whatsapp import BaseTest
 class TestWhatsApp(BaseTest):
 
     def test_whatsapp_01_send_message(self):
-        introPage = WhatsAppPage(self.driver)
-        introPage.send_message()
+        intro_page = WhatsAppPage(self.driver)
+        intro_page.send_message()
 
 # python3 -m unittest testcase.test_whatsapp_01_send_message
--- Test/testcase/whatsapp/test_whatsapp_02_send_group_message.py
+++ Test/testcase/whatsapp/test_whatsapp_02_send_group_message.py
@@ -5,7 +5,7 @@ from testcase.whatsapp.base_test_whatsapp import BaseTest
 class TestWhatsApp(BaseTest):
 
     def test_whatsapp_02_send_group_message(self):
-        introPage = WhatsAppPage(self.driver)
-        introPage.send_group_message()
+        intro_page = WhatsAppPage(self.driver)
+        intro_page.send_group_message()
 
 # python3 -m unittest testcase.test_whatsapp_02_send_group_message
